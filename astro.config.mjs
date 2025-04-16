da6ed7dba3ba3b064ae548bc449e024c6e115db6
Curetix <me@curetix.eu>
2025-04-16 20:34:04 +0200

    Use Astro Font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Outfit",
+        cssVariable: "--font-outfit",
+      },
+    ],
     responsiveImages: true,
   },
   build: {
